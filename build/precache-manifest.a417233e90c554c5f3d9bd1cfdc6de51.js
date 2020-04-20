ff1aa4b9b44fab5eb02e7e48588390c5a33f049f
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-04-20 00:19:18 -0400

    updated homepage link

---
+++ build/precache-manifest.a417233e90c554c5f3d9bd1cfdc6de51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf63da011cb1c10f8f1186e4e1c808ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5156552771b698aed3fe",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2830be5a226b8c351855",
+    "url": "/static/js/2.21c2b348.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/static/js/2.21c2b348.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5156552771b698aed3fe",
+    "url": "/static/js/main.792b206b.chunk.js"
+  },
+  {
+    "revision": "5db6dfe7a8e2b4135dd2",
+    "url": "/static/js/runtime-main.aa739829.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.6b7957d20cb19cc358d83537ee58fecd.js"
+  "/precache-manifest.a417233e90c554c5f3d9bd1cfdc6de51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
