86360a28517b1a42a3b6ef8c6266fb678fc9f038
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-05-20 21:23:35 -0400

    moved build

---
+++ precache-manifest.b5ee2c822a686dadb155a26461aa3e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa879f3c885b4b2ea77d1cbe0d734afe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a41a477d4220fe8dc2a9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e08fc1fd2e95e582ff7f",
+    "url": "/static/js/2.1fa57787.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fa57787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41a477d4220fe8dc2a9",
+    "url": "/static/js/main.b17bc1d0.chunk.js"
+  },
+  {
+    "revision": "45b35319d056ae14fa8f",
+    "url": "/static/js/runtime-main.527874c5.js"
+  }
+]);
